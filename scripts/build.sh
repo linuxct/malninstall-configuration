e6ebf5d655b508c13ebe30c1c1f0e065a94ccaf2
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-05-11 15:50:00 +0200

    feat: change paths in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git checkout -- . ; git pull && source .env && sudo docker build -t malninstallconfiguration . && sudo docker-compose down && sudo docker-compose up -d
+git checkout -- . ; git pull && source .env && sudo docker build -t malninstallconfiguration ../src/ && sudo docker-compose down && sudo docker-compose up -d
