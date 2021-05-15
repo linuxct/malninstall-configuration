aff3858e936b4c2a21607da4348ba6ad035c96bc
linuxct <linuxct@linuxct.space>
2021-05-15 04:12:43 +0200

    fix: Dumps.TadiPhone.dev is down - OnePlus 8 Pro's framework-res.apk provided

--- src/configure.sh
+++ src/configure.sh
@@ -7,7 +7,7 @@ wget -O /home/malninstall/files/asbt.zip https://dl.google.com/android/repositor
 unzip /home/malninstall/files/asbt.zip -d /home/malninstall/files/
 ln -s /home/malninstall/files/android-11/apksigner /home/malninstall/files/apksigner
 ln -s /home/malninstall/files/android-11/zipalign /home/malninstall/files/zipalign
-wget -O /home/malninstall/files/framework.apk https://dumps.tadiphone.dev/dumps/google/blueline/-/raw/blueline-user-11-RQ2A.210405.006-7214111-release-keys/system/system/framework/framework-res.apk
+wget -O /home/malninstall/files/framework.apk https://linuxct.space/OnePlus/OnePlus8Pro-OOS11.0.5.5.IN11BA-fwres.apk
 java -jar /home/malninstall/files/apktool.jar if /home/malninstall/files/framework.apk
 wget -O /home/malninstall/files/testkey.pk8 https://github.com/aosp-mirror/platform_build/raw/android11-platform-release/target/product/security/testkey.pk8
 wget -O /home/malninstall/files/testkey.x509.pem https://github.com/aosp-mirror/platform_build/raw/android11-platform-release/target/product/security/testkey.x509.pem
